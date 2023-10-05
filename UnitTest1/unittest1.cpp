aec4175148560c3a36d1f6504b6e6b65e2da70d8
alegen-it <alegen@tiscali.it>
2023-10-06 00:32:08 +0200

    Test: implemeted CLASS INITIALIZE and CLEANUP, added test priorities

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -7,66 +7,92 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {		
+	
+	alegen_it::database::Query *mpQuery;
 	TEST_CLASS(UnitTest1)
 	{
+	private:
+
 	public:
-		
+
+		TEST_CLASS_INITIALIZE(TestClassInitialize)
+		{
+			mpQuery = new alegen_it::database::Query();
+			mpQuery->setConnectionParameters(L"localhost", L"database1", L"user1", L"Password1");
+
+		}
+
+		TEST_CLASS_CLEANUP(TestClassCleanup)
+		{
+			mpQuery->~Query();
+		}
+
+		BEGIN_TEST_METHOD_ATTRIBUTE(TestClassInit)
+			TEST_OWNER(L"Alegen")
+			TEST_PRIORITY(1)
+		END_TEST_METHOD_ATTRIBUTE()
+
 		TEST_METHOD(TestClassInit)
 		{
-			alegen_it::database::Query *pQuery = new alegen_it::database::Query();
-			Assert::IsNotNull(pQuery);
-			pQuery->~Query();
+			Assert::IsNotNull(mpQuery);
+			
 		}
 
+		BEGIN_TEST_METHOD_ATTRIBUTE(TestSQLConnection)
+			TEST_OWNER(L"Alegen")
+			TEST_PRIORITY(2)
+		END_TEST_METHOD_ATTRIBUTE()
 		TEST_METHOD(TestSQLConnection)
 		{
-			alegen_it::database::Query *pQuery = new alegen_it::database::Query();
-			pQuery->setConnectionParameters(L"localhost", L"notExist", L"user1", L"Password1");
+			alegen_it::database::Query *pQuery = new alegen_it::database::Query();;
+			pQuery->setConnectionParameters(L"localhost", L"not_existing", L"user1", L"Password1");
 			Assert::IsFalse(pQuery->testConnection(), L"Connection to a wrong server fails");
 			Logger::WriteMessage(pQuery->getMessage().c_str());
-
-			pQuery->setConnectionParameters(L"localhost", L"database1", L"user1", L"Password1");
-			Assert::IsTrue(pQuery->testConnection(), L"Connection to the server succeeds");
-			Logger::WriteMessage(pQuery->getMessage().c_str());
-
-
 			pQuery->~Query();
+
+			Assert::IsTrue(mpQuery->testConnection(), L"Connection to the server succeeds");
+			Logger::WriteMessage(mpQuery->getMessage().c_str());
+
 		}
 
-		TEST_METHOD(TestQuery)
+		BEGIN_TEST_METHOD_ATTRIBUTE(TestQuery)
+			TEST_OWNER(L"Alegen")
+			TEST_PRIORITY(3)
+		END_TEST_METHOD_ATTRIBUTE()
+			TEST_METHOD(TestQuery)
 		{
-			alegen_it::database::Query *pQuery = new alegen_it::database::Query();
-			pQuery->setConnectionParameters(L"localhost", L"database1", L"user1", L"Password1");
 
-			std::wstring result = pQuery->ExecDirect(L"SELECT @@VERSION");
+			std::wstring result = mpQuery->ExecDirect(L"SELECT @@VERSION");
 			Logger::WriteMessage(result.c_str());
 			Assert::IsTrue(result.substr(0, 10) == std::wstring(L"Microsoft SQL Server 2022").substr(0,10), L"The expected version is Microsoft SQL Server 2022");
+
 		}
 
+		BEGIN_TEST_METHOD_ATTRIBUTE(TestCreate)
+			TEST_OWNER(L"Alegen")
+			TEST_PRIORITY(4)
+		END_TEST_METHOD_ATTRIBUTE()
 		TEST_METHOD(TestCreate)
 		{
-			alegen_it::database::Query *pQuery = new alegen_it::database::Query();
-			pQuery->setConnectionParameters(L"localhost", L"database1", L"user1", L"Password1");
 
 			Logger::WriteMessage("Drop table ...");
-			std::wstring result = pQuery->ExecDirect(L"DROP TABLE dbo.TABLE2");
-			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + pQuery->getMessage()).c_str());
+			std::wstring result = mpQuery->ExecDirect(L"DROP TABLE dbo.TABLE2");
+			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + mpQuery->getMessage()).c_str());
 
 			Logger::WriteMessage("Create table ...");
-			result = pQuery->ExecDirect(L"CREATE TABLE dbo.TABLE2 (ID INT NOT NULL)");
-			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + pQuery->getMessage()).c_str());
+			result = mpQuery->ExecDirect(L"CREATE TABLE dbo.TABLE2 (ID INT NOT NULL)");
+			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + mpQuery->getMessage()).c_str());
 
 			Assert::IsTrue(result == std::wstring(L""),  L"Result should be empty");
 
 			Logger::WriteMessage("Drop table ...");
-			result = pQuery->ExecDirect(L"DROP TABLE dbo.TABLE2");
-			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + pQuery->getMessage()).c_str());
+			result = mpQuery->ExecDirect(L"DROP TABLE dbo.TABLE2");
+			Logger::WriteMessage((L"... done. Result: " + result + L" - Message: " + mpQuery->getMessage()).c_str());
 
 			Assert::IsTrue(result == std::wstring(L""), L"Result should be empty");
 
-
-
 		}
+
 
 
 	};
